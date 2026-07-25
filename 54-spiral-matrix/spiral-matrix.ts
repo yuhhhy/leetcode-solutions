6e4ab11ce7afdeba9372c6aee846fb9c8de581bf
一曝十寒 <148607309+yuhhhy@users.noreply.github.com>
2026-07-25 12:11:11 +0800

    solve: 54 spiral-matrix (typescript)

--- 54-spiral-matrix/spiral-matrix.ts
+++ 54-spiral-matrix/spiral-matrix.ts
@@ -1,35 +1,36 @@
 // Problem: https://leetcode.cn/problems/spiral-matrix/
-// Accepted at: 2026年3月24日 14:42
+// Accepted at: 2026年7月25日 12:11
 
 function spiralOrder(matrix: number[][]): number[] {
-    const m = matrix.length;
-    const n = matrix[0].length;
     const result: number[] = [];
-    let top = 0;
-    let bottom = m - 1;
-    let left = 0;
-    let right = n - 1;
+    const m = matrix.length, n = matrix[0].length
+    let left = 0, right = n - 1, top = 0, bottom = m - 1;
+
     while (left <= right && top <= bottom) {
-        for (let col = left; col <= right; col++) {
-            result.push(matrix[top][col]);
-        };
-        top++
-        for (let row = top; row <= bottom; row++) {
-            result.push(matrix[row][right]);
-        };
+        for (let i = left; i <= right; i++) {
+            result.push(matrix[top][i]);
+        }
+        top++;
+
+        for (let i = top; i <= bottom; i++) {
+            result.push(matrix[i][right]);
+        }
         right--;
+
         if (top <= bottom) {
-            for (let col = right; col >= left; col--) {
-                result.push(matrix[bottom][col]);
+            for (let i = right; i >= left; i--) {
+                result.push(matrix[bottom][i]);
             }
             bottom--;
         }
+
         if (left <= right) {
-            for (let row = bottom; row >= top; row--) {
-                result.push(matrix[row][left]);
+            for (let i = bottom; i >= top; i--) {
+                result.push(matrix[i][left]);
             }
             left++;
         }
     }
+
     return result;
 };
