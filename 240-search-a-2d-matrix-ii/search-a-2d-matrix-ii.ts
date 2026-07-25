0f213af8dd772ac25480bb7c0cb70c1901c585f5
一曝十寒 <148607309+yuhhhy@users.noreply.github.com>
2026-07-25 15:25:52 +0800

    solve: 240 search-a-2d-matrix-ii (typescript)

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.ts
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.ts
@@ -1,19 +1,20 @@
 // Problem: https://leetcode.cn/problems/search-a-2d-matrix-ii/
-// Accepted at: 2026年3月7日 16:10
+// Accepted at: 2026年7月25日 15:25
 
 function searchMatrix(matrix: number[][], target: number): boolean {
-    // 从右上角开始遍历
     const m = matrix.length;
     const n = matrix[0].length;
     let i = 0;
     let j = n - 1;
+
     while (i < m && j >= 0) {
-        if (matrix[i][j] === target) {
+        const current = matrix[i][j];
+        if(current === target){
             return true;
-        } else if (matrix[i][j] > target) {
-            j--;
-        } else {
+        }else if (current < target){
             i++;
+        }else{
+            j--;
         }
     }
     return false;
