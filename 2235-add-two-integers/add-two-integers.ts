2c3d2ee4275ea1aa45bdcfb54e7deeda78ff8c6a
一曝十寒 <148607309+yuhhhy@users.noreply.github.com>
2026-09-05 13:06:41 +0800

    solve: 2235 add-two-integers (typescript)

--- 2235-add-two-integers/add-two-integers.ts
+++ 2235-add-two-integers/add-two-integers.ts
@@ -1,6 +1,17 @@
 // Problem: https://leetcode.cn/problems/add-two-integers/
-// Accepted at: 2026年9月5日 11:30
+// Accepted at: 2026年9月5日 12:48
 
 function sum(num1: number, num2: number): number {
-    return num1 + num2;
-};
+    while (num2 !== 0) {
+        // 计算进位
+        const carry = (num1 & num2) << 1;
+
+        // 异或得到不考虑进位的结果
+        num1 = num1 ^ num2;
+
+        // 下一轮处理进位
+        num2 = carry;
+    }
+
+    return num1;
+}
